bf2688f0fb6ca0f03c88bee734acd0614f631dc7
Malte Schroeder <post@malte-schroeder.de>
2019-03-28 12:42:14 +0100

    Update json-pass.php
    All requests handled via Curl now. Previous method caused open connections remaining on mAirlist only closed by timeout.

--- json-pass.php
+++ json-pass.php
@@ -5,7 +5,7 @@
  * @package     
  * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
  * @author      Malte Schroeder <post@malte-schroeder.de>
- * @copyright   Copyright (c) 2017-2018 Malte Schroeder (http://www.malte-schroeder.de)
+ * @copyright   Copyright (c) 2017-2019 Malte Schroeder (http://www.malte-schroeder.de)
  *
  */
  
@@ -21,10 +21,31 @@ ini_set('default_socket_timeout', 2); // kein Response in 2 Sekunden, dann ist i
 if (isset($_GET['read'])) 
 	{
 		$read = ($_GET['read']);
-		$json = file_get_contents('http://'.$mairlistuser.':'.$mairlistpass.'@'.$mairlistserver.':'.$mairlistrestport.$read);
-		header('Content-Type: application/json');
-		header("Access-Control-Allow-Origin: *");
+		header('Content-Type: application/json; charset=UTF-8');
+		header("Access-Control-Allow-Origin: *");		
+		$ch = curl_init();
+		$timeout = 2; // 0 wenn kein Timeout
+		curl_setopt($ch, CURLOPT_URL, 'https://'.$mairlistserver.':'.$mairlistrestport.$read);
+		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($ch, CURLOPT_USERPWD, $mairlistuser.':'.$mairlistpass);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_FAILONERROR, TRUE);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$json = curl_exec($ch);
+		if (curl_exec($ch) === FALSE)
+			{
+			  $response_array['status'] = 'error';
+			  $response_array['result'] =  curl_error($ch).', error code: '.curl_errno($ch);
+			  $json = json_encode($response_array);
+			}
+		curl_close($ch);
+		header('Content-Length: '.strlen($json));
 		echo $json;
+	
+		
+		
+		
 	}
 else if (isset($_POST)) 
 		{
@@ -40,13 +61,15 @@ else if (isset($_POST))
 	
 function postrest($server, $port, $user, $pass, $execcommand)
 {		
-		$fp = curl_init($server.':'.$port.'/execute');
+		$fp = curl_init();
+		curl_setopt($fp,CURLOPT_URL, 'https://'.$server.':'.$port.'/execute');
 		curl_setopt($fp,CURLOPT_TIMEOUT,2);
 		curl_setopt($fp,CURLOPT_FAILONERROR,0);
 		curl_setopt($fp,CURLOPT_RETURNTRANSFER,1);
 		curl_setopt($fp,CURLOPT_HTTPAUTH, CURLAUTH_BASIC); 
 		curl_setopt($fp,CURLOPT_USERPWD, $user.':'.$pass); 
 		curl_setopt($fp,CURLOPT_POST, 1); 
+		curl_setopt($fp, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($fp,CURLOPT_POSTFIELDS, 'command='.urlencode($execcommand));
 		curl_exec($fp);
 		echo $_POST['command'];
